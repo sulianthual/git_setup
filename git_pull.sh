dfb8cfd0d6299c9407715dd52eaeea0aa59b9a43
sulianthual <sulian.thual@gmail.com>
2025-03-14 10:55:21 -0400

    20250314-105521

---
+++ git_pull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Script for pull repository changes (follow instructions)
+## chmod +x git_pull.sh
+## ./git_pull.sh
+# This script is so basic you should just enter the command yourself
+
+git pull
